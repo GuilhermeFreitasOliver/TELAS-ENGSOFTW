2258f90870e8fe114e9dcf8cf2f06738827e4bf1
Guilherme Freitas <guiolivers15@gmail.com>
2025-02-25 22:44:47 -0300

    mudança

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
 
 function App() {
   useEffect(() => {
-    // Executa AOS após um pequeno atraso para 
+    // Executa AOS após um pequeno atraso para
     setTimeout(() => {
       AOS.init({ duration: 500, once: true });
     }, 300);
@@ -19,7 +19,6 @@ function App() {
 
   return (
     <Router>
-      <Suspense fallback={<div>Carregando...</div>}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
